a926d19a42e144568ee751285a364ff7fe25313d
Brandon Perard <brandon.perard@gmail>
2021-03-22 22:13:32 -0700

    add copy-and-push test.js with test

---
+++ copy-and-push.test.js
@@ -0,0 +1,8 @@
+const copyAndPush = require('./copy-and-push');
+
+describe('returns a new array with all the items in the original array and a new item pushed to the end.', () => {
+    it('takes in array and returns copy with "4" at end', () => {
+
+        expect(copy).toEqual([1, 2, 3, 4]);
+    });
+});
