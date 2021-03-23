1ef59d7aef199399c611db679345b409abf9f1d7
Brandon Perard <brandon.perard@gmail>
2021-03-22 22:43:31 -0700

    red commit, add test to capitalize-and-filter.test.js

---
+++ capitalize-and-filter.test.js
@@ -0,0 +1,17 @@
+const capitalizeAndFilter = require('capitalize-and-filter');
+
+describe('filters and capitalizes array of strings', () => {
+    it('filters strings starting with "F" and capitalizes the rest', () => {
+        const strings = [
+            'yollo',
+            'food',
+            'potato',
+            'jeep',
+            'flapjack',
+        ]
+
+        const filtered = capitalizeAndFilter(strings);
+
+        expect(filtered).toEqual(['Yollo', 'Potato', 'Jeep']);
+    });
+});
